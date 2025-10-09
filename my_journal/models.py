b1f2b4ec454fedf383ccf59e78279a726e03443f
Donovan Campbell <donovan@dlcentertainment.net>
2025-10-08 20:46:48 -0700

    home template in progress

--- my_journal/models.py
+++ my_journal/models.py
@@ -1,3 +1,16 @@
+import datetime
+
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class Journal(models.Model):
+    title = models.CharField('Optional title', max_length=100)
+    entry = models.TextField()
+    pub_date = models.DateTimeField('date published')
+    
+    def __str__(self):
+        return self.title
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
--- my_journal/views.py
+++ my_journal/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def home(request):
+    return HttpResponse('<h1>Welcome to Jazzy Journal!</h1>')
+
+def journal_index(request):
+    return HttpResponse('<h1>And here lies all of your Journal Entries</h1>')
