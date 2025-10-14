26bb28290acb5a40fcb76a2d727a4a6040af2136
Donovan Campbell <donovan@dlcentertainment.net>
2025-10-14 15:46:28 -0700

    detail page finished

--- my_journal/views.py
+++ my_journal/views.py
@@ -10,5 +10,6 @@ def journal_index(request):
     journal_entries = Journal.objects.all()
     return render(request, 'journal/index.html', { 'journal_entries': journal_entries })
 
-def journal_detail(request):
-    return HttpResponse('<h1>And here is the specific journal entry you clicked on</h1>')
+def journal_detail(request, journal_id):
+    journal_entry = Journal.objects.get(id=journal_id)
+    return render(request, 'journal/detail.html', { 'journal_entry': journal_entry })
