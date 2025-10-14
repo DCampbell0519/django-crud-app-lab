7daa1fbc4aa1e83e183b4040625ac2002a776a80
Donovan Campbell <donovan@dlcentertainment.net>
2025-10-13 18:11:20 -0700

    landing and index views, mild css

--- my_journal/urls.py
+++ my_journal/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('journal/', views.journal_index, name='journal-index')
+    path('journal/', views.journal_index, name='journal-index'),
+    path('journal/<int:journal_id>/', views.journal_detail, name='journal-detail'),
 ]
--- my_journal/views.py
+++ my_journal/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Journal
 
 # Create your views here.
 def home(request):
-    return HttpResponse('<h1>Welcome to Jazzy Journal!</h1>')
+    return render(request, 'home.html')
 
 def journal_index(request):
-    return HttpResponse('<h1>And here lies all of your Journal Entries</h1>')
+    journal_entries = Journal.objects.all()
+    return render(request, 'journal/index.html', { 'journal_entries': journal_entries })
+
+def journal_detail(request):
+    return HttpResponse('<h1>And here is the specific journal entry you clicked on</h1>')
